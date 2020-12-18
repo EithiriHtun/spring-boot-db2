31e2ee89cc67e1efff41d7c68eed9ec3b63fb3e2
eithiri <eithiritun1997@gmail.com>
2020-12-18 17:24:00 +0630

    Initial commit

---
+++ src/main/java/springDb2/springframework/converters/ProductToProductForm.java
@@ -0,0 +1,22 @@
+package springDb2.springframework.converters;
+
+import springDb2.springframework.commands.ProductForm;
+import springDb2.springframework.domain.Product;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+@Component
+public class ProductToProductForm implements Converter<Product, ProductForm> {
+    @Override
+    public ProductForm convert(Product product) {
+        ProductForm productForm = new ProductForm();
+        productForm.setId(product.getId());
+        productForm.setDescription(product.getDescription());
+        productForm.setPrice(product.getPrice());
+        productForm.setImageUrl(product.getImageUrl());
+        return productForm;
+    }
+}
---
+++ src/main/java/springDb2/springframework/services/ProductService.java
@@ -0,0 +1,22 @@
+package springDb2.springframework.services;
+
+import springDb2.springframework.commands.ProductForm;
+import springDb2.springframework.domain.Product;
+
+import java.util.List;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+public interface ProductService {
+
+    List<Product> listAll();
+
+    Product getById(Long id);
+
+    Product saveOrUpdate(Product product);
+
+    void delete(Long id);
+
+    Product saveOrUpdateProductForm(ProductForm productForm);
+}
